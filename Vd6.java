6573b399b13dceb9e009b75c0b01558b06cbc673
hung2504 <43133441+hung2504@users.noreply.github.com>
2018-09-15 17:51:22 +0700

    Add files via upload

--- Vd6.java
+++ Vd6.java
@@ -5,14 +5,8 @@ import java.util.Scanner;
 
 public class Vd6 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Nhap so a:");
-        int a = sc.nextInt();
-        System.out.println("Nhap so b:");
-        int b = sc.nextInt();
         Random rand = new Random();
-        for (int i = 0; i < (b - a); i++) {
-            System.out.println(rand.nextInt(b + 1 - a) + " ");
-        }
+        int ngaunhien = rand.nextInt(100);
+        System.out.println("So ngau nhien la:"+ngaunhien);
     }
 }
